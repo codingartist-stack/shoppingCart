7ebd1f1b3c2d6dc95eb34123b08e9fec734fd181
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-19 12:56:49 -0800

    installed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <>
-      <h1>Shopping Cart</h1>
+      <h1>Shopping</h1>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
     </>
   );
 }
