f956b8695f4466bd9ca48ff4167adff9cd2d6073
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-19 16:57:18 -0800

    created a useState for the products and cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { useState } from 'react';
+import NavBar from './components/NavBar';
+
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [carkItems, setCarkItems] = useState([]);
   return (
     <>
       <h1>Shopping</h1>
-      <nav>
-        <ul>
-          <li></li>
-        </ul>
-      </nav>
+      <NavBar />
     </>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
