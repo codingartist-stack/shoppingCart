86d367af348bae631aefcb4530dff683c619cead
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-22 11:51:48 -0800

    set up a router file

---
+++ src/Router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Products from './components/Products.jsx';
+import Cart from './components/Cart.jsx';
+import App from './App.jsx';
+
+const Router = () => {
+  const router = createBrowserRouter([
+    { path: '/', element: <App /> },
+    {
+      path: 'products',
+      element: <Products />,
+    },
+    {
+      path: 'cart',
+      element: <Cart />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Products from './components/Products.jsx';
-import Cart from './components/Cart.jsx';
 
-const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  {
-    path: 'products',
-    element: <Products />,
-  },
-  {
-    path: 'cart',
-    element: <Cart />,
-  },
-]);
+import './index.css';
+import Router from './Router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </React.StrictMode>
 );
