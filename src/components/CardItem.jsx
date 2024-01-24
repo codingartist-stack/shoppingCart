d0bb44c2575df0aab76673f1b7aad192692646fa
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-23 16:17:04 -0800

    got my numbers about where I want them for the grid. it's working.

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,7 +1,7 @@
 const CardItem = ({ item }) => {
   return (
     <div className="cardItem" key={item.id}>
-      <img src={item.image} alt={item.title} />
+      <img src={item.image} alt={item.title} className="productImage" />
     </div>
   );
 };
