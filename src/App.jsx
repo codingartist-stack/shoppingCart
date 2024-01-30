0aa151752315f2cd314eb840317f70ecb5077c43
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-29 18:09:12 -0800

    starting to put a few logos in to the footer. I wanted to do a hover effect, but it's not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from './components/NavBar';
 import { Outlet } from 'react-router-dom';
+import Footer from './components/footer';
 
 function App({ cartItems }) {
   return (
@@ -7,6 +8,7 @@ function App({ cartItems }) {
       <h1>4FunShopping</h1>
       <NavBar cartItems={cartItems} />
       <Outlet />
+      <Footer />
     </>
   );
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,20 @@
+import facebook from '../assets/svg_icon/fbLogo.svg';
+import redditLogo from '../assets/svg_icon/redditLogoIcon.svg';
+
 export default function Footer() {
   return (
     <>
-      <footer></footer>
+      <footer>
+        <div className="socialIcons">
+          <img
+            src={facebook}
+            alt="facebook Icon"
+            className="footerIcon facebookIcon"
+          />
+          <img src={redditLogo} alt="reddit logo" className="footerIcon" />
+        </div>
+        <div>thank you for visiting</div>
+      </footer>
     </>
   );
 }
