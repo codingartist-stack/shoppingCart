778710a28db7dee617be3284dccb21624298ee79
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-30 11:26:53 -0800

    changed the image of the landing page.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import shopImg from '../assets/images/shopsNight.jpg';
+import storeFront from '../assets/images/storeFront.jpg';
 
 export default function LandingPage() {
   return (
@@ -8,14 +9,14 @@ export default function LandingPage() {
           Discover the Extraordinary: Where Every Shopping Experience is a
           Whimsical Adventure!
         </h2>
-        <img src={shopImg} alt="shops at night" className="landingImg" />
+        <img src={storeFront} alt="shops at night" className="landingImg" />
         <div className="imgInfo">
           Image by{' '}
-          <a href="https://pixabay.com/users/matthiaskost-15902545/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5992277">
-            Matthias Kost
+          <a href="https://pixabay.com/users/bruceemmerling-93533/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=215287">
+            Bruce Emmerling
           </a>{' '}
           from{' '}
-          <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5992277">
+          <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=215287">
             Pixabay
           </a>
         </div>
