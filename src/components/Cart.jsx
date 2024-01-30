92b615ddf31072360d51443d0791f8aaec40643e
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-30 11:56:32 -0800

    I got the pop up working

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,13 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import CartItem from './CartItem';
 
 const Cart = ({ products, cartItems, addToCart, removeFromCart }) => {
   // const [totalPrice, setTotalPrice] = useState(0);
+  const [checkOut, setCheckOut] = useState(false);
+
+  const handleCheckOutMessage = () => {
+    setCheckOut(!checkOut);
+  };
 
   let totalPrice = 0;
 
@@ -17,29 +22,56 @@ const Cart = ({ products, cartItems, addToCart, removeFromCart }) => {
   calculateTotal();
 
   return (
-    <main className="cart">
-      <h1 className="cartTitle">Your Cart Items: {cartItems.length}</h1>
+    <>
+      {checkOut ? (
+        <aside className="CheckOutPop">
+          <div className="checkoutMessage">
+            <button
+              className="iconFont exitBtn"
+              onClick={handleCheckOutMessage}
+            >
+              cross
+            </button>
+            <h3>Check Out</h3>
+            <p>
+              are you ready to check out?
+              <br />
+              Your total is ${totalPrice}
+            </p>
+            <button>Proceed to Checkout</button>
+          </div>
+        </aside>
+      ) : (
+        ''
+      )}
+      <main className="cart">
+        <h1 className="cartTitle">Your Cart Items: {cartItems.length}</h1>
 
-      <p className="total">Total Price: ${totalPrice}</p>
-      <button className="checkOutBtn">Check Out</button>
-      <div className="cartGrid">
-        <hr />
-        {cartItems.map((item) => {
-          return (
-            <>
-              <CartItem
-                item={item}
-                key={crypto.randomUUID()}
-                removeFromCart={removeFromCart}
-              />{' '}
-              <hr />
-            </>
-          );
-        })}
-      </div>
-      <p className="total">Total Price: ${totalPrice}</p>
-      <button className="checkOutBtn">Check Out</button>
-    </main>
+        <p className="total">Total: ${totalPrice}</p>
+        <button className="checkOutBtn" onClick={handleCheckOutMessage}>
+          Check Out
+        </button>
+        <div className="cartGrid">
+          <hr />
+          {cartItems.map((item) => {
+            return (
+              <>
+                <CartItem
+                  item={item}
+                  key={crypto.randomUUID()}
+                  removeFromCart={removeFromCart}
+                />{' '}
+                <hr />
+              </>
+            );
+          })}
+        </div>
+        <p className="total">Total Price: ${totalPrice}</p>
+        <button className="checkOutBtn" onClick={handleCheckOutMessage}>
+          Check Out
+        </button>
+      </main>
+    </>
   );
 };
 
